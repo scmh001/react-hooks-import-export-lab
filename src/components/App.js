404ad54acb91c6c23da355fcf5dc06dbb2e892b7
Shukri Hussein <husseinshukri@outlook.com>
2024-03-10 15:56:15 -0400

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import { username, city, image } from "../data/user.js";
 
 function App() {
   return (
